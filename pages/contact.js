cfaf8bbc10c11eff78a372f8aff2a5cf6f3a173d
Ekansh Jain <95521249+Anonymous-Ekansh@users.noreply.github.com>
2026-06-09 14:40:39 +0530

    Updated contact details

--- pages/contact.js
+++ pages/contact.js
@@ -1,25 +1,28 @@
 import '../styles/routes/contact.scss'
 
-function Contact()
-{
+function Contact() {
     return (
         <div className="ContactUsSection">
             <div className="ContactUsSection__container">
                 <p className='ContactUsSection__container--heading'>Contact Us</p>
                 <div className='ContactUsSection__container--details'>
                     <div className='ContactUsSection__container--details__card'>
-                        <p className='ContactUsSection__container--details__card--name'>Dipro Mukherjee</p>
+                        <p className='ContactUsSection__container--details__card--name'>Rhea Vohra</p>
                         <p className='ContactUsSection__container--details__card--designation'>Lead Organizer</p>
-                        <p className='ContactUsSection__container--details__card--phone'>+91-7032536333</p>
+                        <p className='ContactUsSection__container--details__card--phone'>+91-9810790034</p>
                     </div>
                     <div className='ContactUsSection__container--details__card'>
-                        <p className='ContactUsSection__container--details__card--name'>Aanya Anand</p>
+                        <p className='ContactUsSection__container--details__card--name'>Srinithya Pasupuleti</p>
                         <p className='ContactUsSection__container--details__card--designation'>Co Organizer</p>
-                        <p className='ContactUsSection__container--details__card--phone'>+91-9622816781</p>
+                        <p className='ContactUsSection__container--details__card--phone'>+91-8328251153</p>
                     </div><div className='ContactUsSection__container--details__card'>
-                        <p className='ContactUsSection__container--details__card--name'>Anshika Gupta</p>
+                        <p className='ContactUsSection__container--details__card--name'>Shreyaa Venkataraman</p>
+                        <p className='ContactUsSection__container--details__card--designation'>Managing Director</p>
+                        <p className='ContactUsSection__container--details__card--phone'>+91-9791185259</p>
+                    </div><div className='ContactUsSection__container--details__card'>
+                        <p className='ContactUsSection__container--details__card--name'>Urshita Rathi</p>
                         <p className='ContactUsSection__container--details__card--designation'>Finance Head</p>
-                        <p className='ContactUsSection__container--details__card--phone'>+91-7358012257</p>
+                        <p className='ContactUsSection__container--details__card--phone'>+91-9636896601</p>
                     </div>
                 </div>
             </div>
